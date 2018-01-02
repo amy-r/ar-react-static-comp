332317167d930b4c800bec318287cfa13cded19c
Amy Russell <russell.amyh@gmail.com>
2018-01-02 06:53:46 -0700

    first commit

--- src/Author.js
+++ src/Author.js
@@ -1,8 +1,19 @@
-// Create an Author component
-<div className='story-author-info'>
-  <img src={props.authImg} className='story-author-image' />
-  <div className='story-author-text'>
-    <h4>{props.authName}</h4>
-    <h4>{props.estTime} read</h4>
-  </div>
-</div>
+import React from 'react';
+import './Author.css';
+
+const Author = (storyProps) => {
+  return (
+    <div className='author-info'>
+      <img src={storyProps.authImg} className='author-image' />
+      <div className='author-text'>
+        <h4 className='author-text'>{storyProps.authName}</h4>
+        <h4 className='author-text'>{storyProps.estTime} read</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Author;
+
+
+
--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  // WHAT GOES HERE?
+  return (
+    <div className = "Footer">
+    2017
+    </div>
+  )
 }
 
 export default Footer;
